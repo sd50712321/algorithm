a30198f0606eb85b13df668ae11b007960c60a68
이상민 <sm5445@team-everywhere.com>
2022-03-17 17:40:29 +0900

    크레인 인형 뽑기

--- programmers/크레인인형뽑기.js
+++ programmers/크레인인형뽑기.js
@@ -1,17 +1,29 @@
+function deleteDuplicateStackMember(stack, answer) {
+  console.log('stack', stack);
+  console.log('answer', answer);
+  for(let i = 0; i < stack.length-1 ; i++) {
+    if(stack[i] === stack[i+1]) {
+      answer += 2;
+      stack = stack.filter((_, index) => index !== i && index !== i+1);
+      deleteDuplicateStackMember(stack, answer);
+    }
+  }
+}
+
 function solution(board, moves) {
   let answer = 0;
   let stack = [];
   for (const move of moves) {
     const toys = board[move - 1];
-    while(true) {
-      for (const toy of toys) {
-        if (toy !== 0) {
-          stack.push(toy);
-          toys[toy] = 0;
-          break;
-        }
+    console.log('toys', toys);
+    for(let i = toys.length - 1; i >= 0; i--) {
+      if(toys[i] !== 0) {
+        stack.push(toys[i]);
+        board[move - 1][i] = 0;
+        break;
       }
     }
+    deleteDuplicateStackMember(stack, answer);
   }
   return answer;
 }
