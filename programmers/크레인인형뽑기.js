1209569d846643743944cd4253e9e56c9fe97840
이상민 <sm5445@team-everywhere.com>
2022-03-18 14:34:01 +0900

    크레인 인형뽑기 완료

--- programmers/크레인인형뽑기.js
+++ programmers/크레인인형뽑기.js
@@ -1,30 +1,50 @@
-function deleteDuplicateStackMember(stack, answer) {
-  console.log('stack', stack);
-  console.log('answer', answer);
-  for(let i = 0; i < stack.length-1 ; i++) {
-    if(stack[i] === stack[i+1]) {
-      answer += 2;
-      stack = stack.filter((_, index) => index !== i && index !== i+1);
-      deleteDuplicateStackMember(stack, answer);
-    }
-  }
-}
+// function solution(board, moves) {
+//   let answer = 0;
+//   let stack = [];
+//   for (let i = 0 ; i<moves.length; i++) {
+//     const move = moves[i] - 1;
+//     for(let j = 0 ; j<board.length ; j++) {
+//       if(board[j][move] !== 0) {
+//         stack.push(board[j][move]);
+//         board[j][move] = 0;
+//         if(stack[stack.length-1] === stack[stack.length-2]) {
+//           answer += 2;
+//           stack.pop();
+//           stack.pop();
+//         }
+//         break;
+//       }
+//     }
+//   }
+//   return answer;
+// }
 
-function solution(board, moves) {
-  let answer = 0;
-  let stack = [];
-  for (const move of moves) {
-    const toys = board[move - 1];
-    console.log('toys', toys);
-    for(let i = toys.length - 1; i >= 0; i--) {
-      if(toys[i] !== 0) {
-        stack.push(toys[i]);
-        board[move - 1][i] = 0;
-        break;
-      }
+// best practice
+const transpose = matrix =>
+    matrix.reduce(
+        (result, row) => row.map((_, i) => [...(result[i] || []), row[i]]),
+        []
+    );
+
+const solution = (board, moves) => {
+    const stacks = transpose(board).map(row =>
+        row.reverse().filter(el => el !== 0)
+    );
+    console.log(stacks);
+    const basket = [];
+    let result = 0;
+
+    for (const move of moves) {
+        const pop = stacks[move - 1].pop();
+        if (!pop) continue;
+        if (pop === basket[basket.length - 1]) {
+            basket.pop();
+            result += 2;
+            continue;
+        }
+        basket.push(pop);
     }
-    deleteDuplicateStackMember(stack, answer);
-  }
-  return answer;
-}
+
+    return result;
+};
 console.log(solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]], [1,5,3,5,1,2,1,4]));
