3243ad72a9cd7980051a8e714809323522fc64d7
이상민 <sm5445@team-everywhere.com>
2022-03-22 17:57:46 +0900

    완주하지못한 선수 수정중

--- programmers/완주하지못한선수.js
+++ programmers/완주하지못한선수.js
@@ -1,11 +1,28 @@
 function solution(participant, completion) {
-  const duplicateParticipant = participant.reduce((acc, cur) => {
-    acc[cur] = (acc[cur] || 0) + 1;
+  const participantTotal = participant.reduce((acc, cur) => {
+    if(acc[cur]){
+      acc[cur] += 1;
+    } else {
+      acc[cur] = 1;
+    }
+    return acc
   },{});
-  console.log(duplicateParticipant);
-  let answer = completion.filter(data => {
-    return participant.indexOf(data) === -1;
-  });
-  return answer[0];
+  console.log(participantTotal);
+  const duplicateParticipant = Object.keys(participantTotal).filter(key => participantTotal[key] > 1);
+  if (duplicateParticipant.length > 0) {
+    let duplicateCompletion = completion.filter(data => {
+      return duplicateParticipant.includes(data);
+    });
+    return duplicateCompletion[0];
+  }
+  else {
+    const completionParticipant = participant.filter(data => {
+      if(!completion.includes(data)){
+        return data;
+      }
+    })
+    console.log(completionParticipant);
+    return completionParticipant[0];
+  }
 }
 console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]));
