4a2e76d7e77182cbcbd8a7e8b2b43a81db8d1355
이상민 <sm5445@team-everywhere.com>
2022-04-05 17:42:50 +0900

    모의고사 끝

--- programmers/모의고사.js
+++ programmers/모의고사.js
@@ -14,8 +14,35 @@ function solution(answers) {
       answer[2]++
     }
   }
-  return answer.filter((v,i) => v === Math.max(...answer)).map((v,i) => i+1)
+  const result = []
+  answer.forEach((v,i) => {
+    if(v === Math.max(...answer)) {
+      result.push(i+1)
+    }
+  })
+  return result
 }
-console.log(solution([1,2,3,4,5]))
+
+//best practice
+function solution(answers) {
+  var answer = [];
+  var a1 = [1, 2, 3, 4, 5];
+  var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
+  var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
+  var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
+  var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
+  var max = Math.max(a1c,a2c,a3c);
+
+  if (a1c === max) {answer.push(1)};
+  if (a2c === max) {answer.push(2)};
+  if (a3c === max) {answer.push(3)};
+
+
+  return answer;
+}
+
+console.log(solution([1,2,3,4,5,1,2,3,4,5,2,3,2,4,2,5,2,1,2,1,2,3,2,4,2,5,2,1,2,3,2,4,2,5]))
 console.log(solution([1,3,2,4,2]))
 console.log(solution([1,3,2,4,3]))
