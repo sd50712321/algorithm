876e97614084af006499e4da9868eb559f17c4d7
SangMin <sd50712321@gmail.com>
2023-02-24 21:23:04 +0900

    격자판 최대합

--- javascript/격자판최대합.js
+++ javascript/격자판최대합.js
@@ -27,6 +27,16 @@ function solution(n, arr) {
   let answer = Number.MIN_SAFE_INTEGER;
   let n = arr.length;
   let sum1 = (sum2 = 0);
+  for (let i = 0; i < n; i++) {
+    sum1 = sum2 = 0;
+    for (let j = 0; j < n; j++) {
+      sum1 += arr[i][j];
+      sum2 += arr[j][i];
+    }
+    answer = Math.max(answer, sum1, sum2);
+  }
+  sum1 = sum2 = 0;
+  for (let i = 0; i < n; i++) {}
 }
 console.log(
   solution(5, [
